f876faf7a925eb9815932448024063c1287ac680
Darren Liew <ldarren@gmail.com>
2018-09-06 07:25:32 +0000

    make cfg a file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const check = require('./index.js')
-check('https://github.com/ldarren/pm2-deploy.git', 'pico-git-check-test', 'src/config', 'live', (err, lint, test) => {
+check('https://github.com/ldarren/pm2-deploy.git', 'pico-git-check-test', 'src/config/dotenv/test/ops.env', 'live', (err, lint, test) => {
 	console.error('error:', err)
 	console.log('lint:', lint)
 	console.log('test:', test)
