0035381becefc9c47b986ebe4ba86233cc3d2ca9
Darren Liew <ldarren@gmail.com>
2018-09-06 05:05:20 +0000

    added custom dotenv support

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const check = require('./check.js')
-check('https://github.com/ldarren/pm2-deploy.git', 'wewe', 'master', (err, lint, test) => {
-	console.log(err)
-	console.log(lint)
-	console.log(test)
-})
+const fs = require('fs')
+const childProcess = require('child_process')
+const path = require('path')
+
+/**
+ * Spawn a modified version of visionmedia/deploy
+ *
+ * @param {string} repo: repo to be tested
+ * @param {string} dst: local repo checkout place
+ * @param {string} ref: branch or tag to be checkout
+ * @callback cb
+ */
+module.exports = function(repo, dst, cfg, ref, cb) {
+	const shellSyntaxCommand = path.join( __dirname.replace(/\\/g, '/'), 'check ') + `${repo} ${dst} ${cfg} ${ref}`
+	const proc = childProcess.spawn('sh', ['-c', shellSyntaxCommand], { stdio: 'inherit' })
+
+	proc.on('error', cb)
+
+	// TODO: listen to stdout instead of passing data by file
+
+	proc.on('close', function (code) {
+		if (code !== 0) return cb(code)
+		const lintRes = fs.readFileSync(`/tmp/${dst}.lint`, 'utf8')
+		const testRes = fs.readFileSync(`/tmp/${dst}.result`, 'utf8')
+		cb(null, lintRes, testRes)
+	});
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+const check = require('./index.js')
+check('https://github.com/ldarren/pm2-deploy.git', 'pico-git-check-test', 'src/config', 'live', (err, lint, test) => {
+	console.error('error:', err)
+	console.log('lint:', lint)
+	console.log('test:', test)
+})
